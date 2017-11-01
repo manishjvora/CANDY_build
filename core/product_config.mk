04b7c89afa15ab43bf6d8615c843d099b1808621
Dan Cartier <nospamdan1@gmail.com>
2017-11-01 17:13:51 -0400

    Make: Where has my Oreo been?

--- core/product_config.mk
+++ core/product_config.mk
@@ -179,9 +179,9 @@ include $(BUILD_SYSTEM)/node_fns.mk
 include $(BUILD_SYSTEM)/product.mk
 include $(BUILD_SYSTEM)/device.mk
 
-# A Gzosp build needs only the Gzosp product makefiles.
-ifneq ($(GZOSP_BUILD),)
-  all_product_configs := $(shell find device -path "*/$(GZOSP_BUILD)/gzosp.mk")
+# A Candy build needs only the Candy product makefiles.
+ifneq ($(CANDY_BUILD),)
+  all_product_configs := $(shell find device -path "*/$(CANDY_BUILD)/candy.mk")
 else
   ifneq ($(strip $(TARGET_BUILD_APPS)),)
   # An unbundled app build needs only the core product makefiles.
@@ -192,9 +192,9 @@ else
   # files in the tree.
   all_product_configs := $(get-all-product-makefiles)
   endif # TARGET_BUILD_APPS
-endif # GZOSP_BUILD
+endif # CANDY_BUILD
 
-ifeq ($(GZOSP_BUILD),)
+ifeq ($(CANDY_BUILD),)
 all_named_products :=
 
 # Find the product config makefile for the current product.
--- core/soong_config.mk
+++ core/soong_config.mk
@@ -16,8 +16,8 @@ endif
 # Create soong.variables with copies of makefile settings.  Runs every build,
 # but only updates soong.variables if it changes
 SOONG_VARIABLES_TMP := $(shell mktemp -u)
-include vendor/gzosp/build/soong/soong_config.mk
-$(SOONG_VARIABLES): FORCE gzosp_soong
+include vendor/candy/build/soong/soong_config.mk
+$(SOONG_VARIABLES): FORCE candy_soong
 	$(hide) (\
 	echo '    "Make_suffix": "-$(TARGET_PRODUCT)",'; \
 	echo ''; \
