41bc8532bb912e31919729463b93a6e1afa10f92
Dan Cartier <nospamdan1@gmail.com>
2018-05-30 01:34:12 -0400

    tools: Add tags for OTA use
    Change-Id: I0f9eef0acbfcb7ae79f41cdbfd06bc397fe54ed6

--- tools/buildinfo.sh
+++ tools/buildinfo.sh
@@ -31,6 +31,9 @@ echo "ro.product.brand=$PRODUCT_BRAND"
 echo "ro.product.name=$PRODUCT_NAME"
 echo "ro.product.device=$TARGET_DEVICE"
 
+echo "ro.candy.device=$TARGET_DEVICE"
+echo "ro.candy.ota.version=Candy-$TARGET_DEVICE-8.1-OFFICIAL-$(date +"%Y%m%d")"
+
 # These values are deprecated, use "ro.product.cpu.abilist"
 # instead (see below).
 echo "# ro.product.cpu.abi and ro.product.cpu.abi2 are obsolete,"
